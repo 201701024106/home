bde5a96adcc1c21d81b46b0d4ac81a787a133422
wangds_ext <wds@ebidding.org.cn>
2026-03-10 14:48:15 +0800

    core: 刷新页面404

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 关键配置：指定仓库名作为基础路径
+  base: '/home/', 
   plugins: [
     vue(),
     vueDevTools(),
